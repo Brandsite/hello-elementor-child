57039ced9890d4b4fa1ea13b33c6a582e0cc64ae
Kristaps F <77058275+Kfishers@users.noreply.github.com>
2023-07-28 00:56:24 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,32 +1,62 @@
 <?php
 
+/**
+ * -----------------------------------------------------------------------------------------------------------
+ */
 define('BR_THEME_VERSION', '1.0.0');
+define('BR_THEME_TEXT_DOMAIN', wp_get_theme()->get('TextDomain'));
+
 function log_stuff($stuff_to_log)
 {
     error_log(print_r($stuff_to_log, true));
 }
 
+/**
+ * -------------------------------------------------------------------------------------------------------------------------------
+ * Add signiture to admin footer
+ */
+add_filter('admin_footer_text', 'footer_admin');
+function footer_admin()
+{
+    echo '<span id="footer-thankyou">' . __('Developed by ', BR_THEME_TEXT_DOMAIN) . '<a href="https://www.brandsite.lv" target="_blank" rel="nofollow">Brandsite.lv</a></span>';
+}
+
+/**
+ * -------------------------------------------------------------------------------------------------------------------------------
+ * Styles & scripts
+ */
+
+//fontend css
+add_action('wp_enqueue_scripts', 'my_enqueue_styles');
 function my_enqueue_styles()
 {
-
     // enqueue parent styles
     wp_enqueue_style('parent-theme', get_template_directory_uri() . '/style.css');
 
     // enqueue child styles
     wp_enqueue_style('child-theme', get_stylesheet_directory_uri() . '/style.css', array(), BR_THEME_VERSION, "all");
 }
-add_action('wp_enqueue_scripts', 'my_enqueue_styles');
+
+//admin css
+add_action('admin_enqueue_scripts', 'admin_style');
+function admin_style()
+{
+    wp_enqueue_style('br-admin-style', get_stylesheet_directory_uri() . '/admin-style.css', [], BR_THEME_VERSION);
+}
 
 
-/*Child theme JS*/
+//child theme JS
+// add_action('wp_enqueue_scripts', 'theme_js');
 // function theme_js()
 // {
 //     wp_enqueue_script('theme_js', get_stylesheet_directory_uri() . '/JS/app.js', array(), BR_THEME_VERSION, "all");
 // }
-// add_action('wp_enqueue_scripts', 'theme_js');
 
 
-//add SVG support
+/**
+ * -----------------------------------------------------------------------------------------------------------
+ * Add SVG support
+ */
 function svg_mime_type($mimes = array())
 {
     $mimes['svg']  = 'image/svg+xml';
@@ -35,20 +65,23 @@ function svg_mime_type($mimes = array())
 }
 add_filter('upload_mimes', 'svg_mime_type');
 
-//Optimize speed and remove the unwanted stuff
-require_once get_theme_file_path() . '/BrOptimize.php';
 
+/**
+ * -----------------------------------------------------------------------------------------------------------
+ * Optimize speed and remove the unwanted stuff
+ */
+require_once get_theme_file_path() . '/BrOptimize.php';
 $br_optimize = new BrOptimize(array(
-    'emoji'                 => true,
-    'oembed'                => true,
-    'jQuery_migrate'        => true,
-    'new_content_admin_bar' => true,
-    'archive_admin_bar'     => true,
-    'editor_page'           => true, //remove editor from pages
-    'comments_admin_menu'   => true,
-    'comments_admin_bar'    => true,
-    'comment_support_page'  => true,
-    'comment_support_post'  => true,
+    'emoji'                 => false,
+    'oembed'                => false,
+    'jQuery_migrate'        => false,
+    'new_content_admin_bar' => false,
+    'archive_admin_bar'     => false,
+    'editor_page'           => false, //remove editor from pages
+    'comments_admin_menu'   => false,
+    'comments_admin_bar'    => false,
+    'comment_support_page'  => false,
+    'comment_support_post'  => false,
     'defer_javascript'      => false,
 
     //exceptions based on .js file URL. add a file name, full URL or part of the URL
@@ -61,7 +94,62 @@ $br_optimize = new BrOptimize(array(
 
     //exceptions based on page. add page ID - e.g. 66 or page title - e.g. 'Contacts page'
     'defer_exceptions_page' => array(
-//         'Blogs'
+        //         'Blogs'
     )
 ));
 $br_optimize->hooks();
+
+
+/**
+ * -----------------------------------------------------------------------------------------------------------
+ * Custom post types 
+ *-----------------------
+ * Example for creating post-type "Projekti" with custom taxonomies
+ */
+// require_once get_theme_file_path() . '/BrPostType.php';
+// $portfolio = new BrPostType(
+//     array(
+//         'name'          => 'Projekti',
+//         'singular_name' => 'Projekts',
+//         'slug'          => 'projekts',
+//         'menu_position' => 3,
+//         'menu_icon'     => 'dashicons-portfolio',
+//         'taxonomies'    => array(
+//             array(
+//                 'singular_name'       => 'Valsts',
+//                 'plural_name'         => 'Valstis',
+//                 'slug'                => 'valsts',
+//                 'hierarchical'        => true,
+//             ),
+//             array(
+//                 'singular_name'       => 'Kategorija',
+//                 'plural_name'         => 'Kategorijas',
+//                 'slug'                => 'kategorija',
+//                 'hierarchical'        => true,
+//             )
+//         )
+//     )
+// );
+// $portfolio->hooks();
+
+/**
+ * -----------------------------------------------------------------------------------------------------------
+ */
+// add_filter('register_post_type_args', 'remove_default_post_type', 0, 2);
+// function remove_default_post_type($args, $postType)
+// {
+//     if ($postType === 'post') {
+//         $args['public']                = false;
+//         $args['show_ui']               = false;
+//         $args['show_in_menu']          = false;
+//         $args['show_in_admin_bar']     = false;
+//         $args['show_in_nav_menus']     = false;
+//         $args['can_export']            = false;
+//         $args['has_archive']           = false;
+//         $args['exclude_from_search']   = true;
+//         $args['publicly_queryable']    = false;
+//         $args['show_in_rest']          = false;
+//     }
+
+//     return $args;
+// }
